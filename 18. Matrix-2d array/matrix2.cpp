c0644f7d3cfa2c19e95213e56de1da85e54f3dfc
Efraim Manurung <frm199911@gmail.com>
2024-02-14 22:58:26 +0100

    Made some functions for matrix. There are two functions implemented, first with fixed rows and columns and second with custom values. In this program I also learned about calloc and free in C. Also learned about new and delete in C++.

---
+++ 18. Matrix-2d array/matrix2.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const int MATRIX_ROWS = 5;
+const int MATRIX_COLS = 4;
+
+// void print_matrix(int rows, int cols, int mat[rows][cols]) {
+//     for (int row=0; row < rows; row++) {
+//         printf("%d:\t", row);
+//         for (int col=0; col < cols; col++) {
+//             printf("%x ", mat[row][col]);
+//         }
+//         printf("\n");
+//     }
+// }
+
+void print_matrix_2(int rows, int cols, int **mat) {
+    for (int row=0; row < rows; row++) {
+        printf("%d:\t", row);
+        for (int col=0; col < cols; col++) {
+            printf("%x ", mat[row][col]);
+        }
+        printf("\n");
+    }
+}
+
+int main(int argc, char **argv)
+{
+    //int matrix[MATRIX_ROWS][MATRIX_COLS];
+    //int *matrix[MATRIX_ROWS];
+    //int **matrix = (int **) calloc(MATRIX_ROWS, sizeof(int*));
+    int **matrix = new int*[MATRIX_ROWS];
+
+    for (int row=0; row < MATRIX_ROWS; row++) {
+        //matrix[row] = (int *)calloc(MATRIX_ROWS, sizeof(int));
+        matrix[row] = new int[MATRIX_COLS];
+        for (int col=0; col < MATRIX_COLS; col++) {
+            matrix[row][col] = (row << 16) | col;
+        }
+    }
+
+    printf("sizeof(matrix) = %lu\n", sizeof(matrix));
+    //print_matrix(MATRIX_ROWS, MATRIX_COLS, matrix);
+    print_matrix_2(MATRIX_ROWS, MATRIX_COLS, matrix);
+
+    for (int row=0; row < MATRIX_ROWS; row++) {
+        delete[] (matrix[row]);
+    }
+
+    delete[] matrix;
+}
